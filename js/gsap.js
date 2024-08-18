72f3e9d34204489f55d6e8bb162c96ecb77b7845
Lukas Bronski <109726913+webski-dev@users.noreply.github.com>
2024-08-18 20:53:42 +0200

    Add files via upload

---
+++ js/gsap.js
@@ -0,0 +1,71 @@
+gsap.registerPlugin(ScrollTrigger);
+
+var line1 = document.getElementById("image1");
+var line2 = document.getElementById("image2");
+var line3 = document.getElementById("image3");
+var line4 = document.getElementById("image4");
+
+gsap.fromTo(
+  line1,
+  {
+    x: "-100%",
+    opacity: 0,
+  },
+  {
+    x: "0%",
+    opacity: 1,
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".header-image-content",
+      scrub: true,
+    },
+  }
+);
+gsap.fromTo(
+  line2,
+  {
+    x: "100%",
+    opacity: 0,
+  },
+  {
+    x: "0%",
+    opacity: 1,
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".article-2-container",
+      scrub: true,
+    },
+  }
+);
+gsap.fromTo(
+  line3,
+  {
+    x: "-100%",
+    opacity: 0,
+  },
+  {
+    x: "0%",
+    opacity: 1,
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".article-3-container",
+      scrub: true,
+    },
+  }
+);
+gsap.fromTo(
+  line4,
+  {
+    y: "-100%",
+    opacity: 1,
+  },
+  {
+    y: "0%",
+    opacity: 1,
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".footer-container",
+      scrub: true,
+    },
+  }
+);
